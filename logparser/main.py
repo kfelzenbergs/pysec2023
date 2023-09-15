1539e91d4820750090161d896d89060b831dac2b
Kristaps Felzenbergs <kristaps.felzenbergs@gmail.com>
2023-09-15 19:10:40 +0300

    example syslog

--- logparser/main.py
+++ logparser/main.py
@@ -3,11 +3,12 @@ from libs.SysLogParser import SysLogParser
 
 authLogParser = AuthLogParser()
 authLogParser.info()
-authLogParser.readFile()
+# authLogParser.readFile()
 
 
 # print(SysLogParser.log_file_path)
 sysLogParser = SysLogParser()
+sysLogParser.readFile()
 # print(sysLogParser.info())
 # print(sysLogParser.getLogFilePath())
 
