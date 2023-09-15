4e7d72cb771a2f9242b4a94d5b23ba61e2590fe7
Kristaps Felzenbergs <kristaps.felzenbergs@gmail.com>
2023-09-15 19:01:42 +0300

    feat: syslog parser

---
+++ logparser/main.py
@@ -0,0 +1,13 @@
+from libs.AuthLogParser import AuthLogParser
+from libs.SysLogParser import SysLogParser
+
+authLogParser = AuthLogParser()
+authLogParser.info()
+authLogParser.readFile()
+
+
+# print(SysLogParser.log_file_path)
+sysLogParser = SysLogParser()
+# print(sysLogParser.info())
+# print(sysLogParser.getLogFilePath())
+
